06de023334ac25aa006f22c2d0d2b751736b797a
Marc Audy <MarcAudy@users.noreply.github.com>
2023-06-28 23:14:44 -0700

    Day 7 Part 2

--- src/day7.nim
+++ src/day7.nim
@@ -1,6 +1,8 @@
 import os
 import re
 import regex
+import sequtils
+import strutils
 
 proc hasABBA(segment: string): bool =
     return segment.contains(re.re"(.)(?!\1)(.)\2\1")
@@ -22,16 +24,42 @@ proc supportsTLS(line: string): bool =
 
     return false
 
+proc findABA(line: string, bounds: seq[Slice[int]], substr: string): bool = 
+    for bound in bounds:
+        if line[bound].contains(substr):
+            return true
+
+    return false
+
+proc supportsSSL(line: string): bool =
+    var m: RegexMatch
+    doAssert line.match(re"([^\[]+)(?:\[([^\]]+)\]([^\[]+))+", m)
+
+    let supernets = concat(m.group(0),m.group(2))
+
+    for bounds in supernets:
+        let segment = line[bounds]
+        for i in 0..segment.len()-3:
+            if segment[i] == segment[i+2] and segment[i] != segment[i+1]:
+                if findABA(line, m.group(1), segment[i+1]&segment[i]&segment[i+1]):
+                    return true
+
+    return false
+
 proc day7*() =
 
     let f = open(os.getAppDir() & "\\..\\input\\day7.txt")
     defer: f.close()
     var line : string
 
-    var supports = 0
+    var supportsTLS = 0
+    var supportsSSL = 0
 
     while f.read_line(line):
         if supportsTLS(line):
-            supports += 1
+            supportsTLS += 1
+        if supportsSSL(line):
+            supportsSSL += 1
 
-    echo $supports
+    echo "PART1: ", $supportsTLS
+    echo "PART2: ", $supportsSSL
