aac77e1c008a3d0a42610a4fb69faadcaf372901
Marc Audy <MarcAudy@users.noreply.github.com>
2023-06-29 10:28:51 -0700

    Day 8 Part 1

---
+++ src/day8.nim
@@ -0,0 +1,67 @@
+import os
+import regex
+import seqUtils
+import strutils
+import std/bitops
+
+const GridHeight = 6
+const GridWidth = 50
+
+proc drawGrid(grid: array[GridHeight, uint64]) =
+    for row in 0..<GridHeight:
+        var gridLine = ""
+        for col in 0..<GridWidth:
+            gridLine &= (if (grid[row] and (1u64 shl col)) != 0: '#' else: '.')
+        echo gridLine
+    echo ""
+
+proc day8*() =
+
+    let f = open(os.getAppDir() & "\\..\\input\\day8.txt")
+    defer: f.close()
+    var line: string
+
+    var grid: array[GridHeight, uint64]
+
+    while f.read_line(line):
+        var m: RegexMatch
+        if line.match(re"rect (\d+)x(\d+)", m):
+            let rows = parseInt(m.groupFirstCapture(1,line))
+            let cols = parseInt(m.groupFirstCapture(0,line))
+
+            for x in 0..<cols:
+                for y in 0..<rows:
+                    grid[y].setBit(x)
+
+        else:
+            doAssert line.match(re"rotate (\w+ .)=(\d+) by (\d+)", m)
+
+            if m.groupFirstCapture(0, line) == "row y":
+                let row = parseInt(m.groupFirstCapture(1,line))          
+                let by = parseInt(m.groupFirstCapture(2,line)) %% GridWidth
+                let newC0 = GridWidth - by
+                let newStartMask = grid[row].bitsliced(newC0..<GridWidth)
+                grid[row].bitslice(0..<newC0)
+                grid[row] = grid[row].rotateLeftBits(by)
+                grid[row].setMask(newStartMask)
+            else:
+                doAssert m.groupFirstCapture(0, line) == "column x"
+                let col = parseInt(m.groupFirstCapture(1,line))
+                let by = (parseInt(m.groupFirstCapture(2,line)) %% GridHeight)
+                var colBits: uint64
+                for row in 0..<GridHeight:
+                    if grid[row].testBit(col):
+                        colBits.setBit(row)
+                for row in 0..<GridHeight:
+                    # Add GridHeight because mod of a negative number doesn't give us the desired value
+                    if colBits.testBit((row-by+GridHeight)%%GridHeight):
+                        grid[row].setBit(col)
+                    else:
+                        grid[row].clearBit(col)
+                
+        #drawGrid(grid)
+
+    var enabledBits = 0
+    grid.apply(proc (row: uint64) = enabledBits += row.countSetBits())
+
+    echo $enabledBits
--- src/main.nim
+++ src/main.nim
@@ -1,4 +1,4 @@
-import day7
+import day8
 #[import std/times
 proc timeFunction(f: proc(), count: int = 1): float =
     let startTime = cpuTime()
@@ -8,4 +8,4 @@ proc timeFunction(f: proc(), count: int = 1): float =
     return endTime - startTime
 ]#
 
-day7()
+day8()
