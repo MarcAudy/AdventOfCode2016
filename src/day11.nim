f085a40b748afa6a54f13d50660da46e0c724e89
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-10 14:13:16 -0700

    Day 12 Part 1

--- src/day11.nim
+++ src/day11.nim
@@ -2,8 +2,10 @@ import bitops
 import lists
 import sets
 import sequtils
+import tables
 
-const DAY11_USE_BITMASK = 0
+const DAY11_USE_BITMASK = 1
+const DAY11_USE_SCORING = 1
 
 #const DAY11_SAMPLE = 0
 #const DAY11_PART1 = 0
@@ -34,6 +36,11 @@ else:
     type Building = array[4, Floor]
 type State = tuple[building: Building, steps: int]
 
+when declared(DAY11_USE_SCORING):
+    type StateStore = seq[seq[State]]
+else:
+    type StateStore = SinglyLinkedList[State]
+
 when declared(DAY11_SAMPLE):
     let initialBuildingDefinition: BuildingDefinition =
         [{Elevator, RutheniumMicrochip, ThuliumMicrochip},
@@ -55,8 +62,6 @@ when declared(DAY11_PART2):
         {PromethiumGenerator, PromethiumMicrochip, RutheniumGenerator, RutheniumMicrochip},
         {}]
 
-var seenBuildings: HashSet[Building]
-
 when declared(DAY11_USE_BITMASK):
     const FloorMask: Floor = {ObjectTypes.low..ObjectTypes.high}
 
@@ -64,7 +69,8 @@ const GeneratorMask: Floor = {PlutoniumGenerator,PromethiumGenerator,RutheniumGe
 
 proc getFloor(building: Building, floor: int): Floor =
     when declared(DAY11_USE_BITMASK):
-        return cast[Floor](bitand(cast[uint64](FloorMask), building shr cast[uint64](floor * (cast[int](ObjectTypes.high)+1))))
+        #return cast[Floor](bitand(cast[uint64](FloorMask), building shr cast[uint64](floor * (cast[int](ObjectTypes.high)+1))))
+        let sliceStart = floor * (cast[int](ObjectTypes.high)+1)
     else:
         return building[floor]
 
@@ -123,6 +129,15 @@ iterator generateNewFloors(fromFloor: Floor, toFloor: Floor): tuple[newFrom: Flo
             if validFloor(twoObjTo):
                 yield (oneObjFrom-{fromObjects[j]},twoObjTo)
 
+when declared(DAY11_USE_SCORING):
+    proc scoreBuilding(building: Building): int =
+        var buildingScore = 0
+
+        for floor in 0..3:
+            buildingScore += len(building.getFloor(floor)) * floor
+
+        return buildingScore
+
 proc convertDefinition(definition: BuildingDefinition): Building =
     when declared(DAY11_USE_BITMASK):
         var building: Building
@@ -141,21 +156,45 @@ proc convertBuilding(building: Building): BuildingDefinition =
     else:
         return cast[BuildingDefinition](building)
 
+when declared(DAY11_USE_SCORING):
+    proc add(stateStore: var StateStore, state: State) =
+        let stateScore = scoreBuilding(state.building)
+        if stateScore >= len(stateStore):
+            stateStore.setLen(stateScore+1)
+        stateStore[stateScore].add(state)
+
+proc pop(stateStore: var StateStore): State =
+    when declared(DAY11_USE_SCORING):
+        let state = stateStore[stateStore.len()-1].pop()
+        while stateStore.len() > 0 and stateStore[stateStore.len()-1].len() == 0:
+            stateStore.setLen(stateStore.len()-1)
+        return state
+    else:
+        let state = stateStore.head.value
+        stateStore.remove(stateStore.head)
+        return state
+
+
 proc day11*() =
 
     let initialBuilding = convertDefinition(initialBuildingDefinition)
-
     let goal = bitor(cast[int](initialBuilding.getFloor(0)),cast[int](initialBuilding.getFloor(1)),cast[int](initialBuilding.getFloor(2)),cast[int](initialBuilding.getFloor(3)))
 
-    seenBuildings.incl(initialBuilding)
-    var statesToConsider: SinglyLinkedList[State] 
+    when declared(DAY11_USE_SCORING):
+        var seenBuildings = {initialBuilding:0}.toTable
+    else:
+        var seenBuildings: HashSet[Building]
+        seenBuildings.incl(initialBuilding)
+
+    var statesToConsider: StateStore
     statesToConsider.add((initialBuilding,0))
 
-    block mainLoop:
-        while statesToConsider.head != nil:
+    var foundSolution = -1
 
-            let curState = statesToConsider.head.value
-            statesToConsider.remove(statesToConsider.head)
+    block mainLoop:
+        while true:
+
+            let curState = statesToConsider.pop()
 
             let elevatorFloor = curState.building.getElevatorFloor()
 
@@ -163,15 +202,24 @@ proc day11*() =
                 for newFloors in generateNewFloors(curState.building.getFloor(elevatorFloor), curState.building.getFloor(elevatorFloor + 1)):
                     
                     if elevatorFloor == 2 and cast[int](newFloors.newTo) == goal:
+                        if 
                         echo "PART1: " & $(curState.steps + 1)                       
                         break mainLoop
 
                     var newState = curState
                     newState.building.setFloor(elevatorFloor, newFloors.newFrom)
                     newState.building.setFloor(elevatorFloor+1, newFloors.newTo)
-                    if not seenBuildings.containsOrIncl(newState.building):
+                    when declared(DAY11_USE_SCORING):
                         newState.steps += 1
-                        statesToConsider.add(newState)
+                        let seenAtStep = seenBuildings.getOrDefault(newState.building, -1)
+                        if seenAtStep == -1 or seenAtStep > newState.steps:
+                            seenBuildings[newState.building] = newState.steps
+                            statesToConsider.add(newState)
+                    else:
+                        if not seenBuildings.containsOrIncl(newState.building):
+                            newState.steps += 1
+                            statesToConsider.add(newState)                        
+                        
 
             if elevatorFloor > 0:
                 for newFloors in generateNewFloors(curState.building.getFloor(elevatorFloor), curState.building.getFloor(elevatorFloor - 1)):
@@ -179,7 +227,13 @@ proc day11*() =
                     var newState = curState
                     newState.building.setFloor(elevatorFloor, newFloors.newFrom)
                     newState.building.setFloor(elevatorFloor-1, newFloors.newTo)
-                    if not seenBuildings.containsOrIncl(newState.building):
+                    when declared(DAY11_USE_SCORING):
                         newState.steps += 1
-                        statesToConsider.add(newState)
-        
+                        let seenAtStep = seenBuildings.getOrDefault(newState.building, -1)
+                        if seenAtStep == -1 or seenAtStep > newState.steps:
+                            seenBuildings[newState.building] = newState.steps
+                            statesToConsider.add(newState)
+                    else:
+                        if not seenBuildings.containsOrIncl(newState.building):
+                            newState.steps += 1
+                            statesToConsider.add(newState)          
---
+++ src/day12.nim
@@ -0,0 +1,88 @@
+import os
+import regex
+import strutils
+
+type Commands = enum
+    Copy,
+    Inc,
+    Dec,
+    JNZ
+
+type ArgumentType = enum None, Value, Register
+type Argument = object
+    case argType: ArgumentType
+    of Value:
+        val: int
+    of Register:
+        register: int
+    else:
+        discard
+
+var registers = [0,0,0,0]
+
+proc newArgument(arg: string): Argument =
+    if arg[0] in {'a','b','c','d'}:
+        return Argument(argType: Register, register: cast[int](arg[0])-cast[int]('a'))
+    else:
+        return Argument(argType: Value, val: parseInt(arg))
+
+proc getValue(arg: Argument): int =
+    case arg.argType:
+    of Value:
+        return arg.val
+    of Register:
+        return registers[arg.register]
+    else:
+        doAssert false
+
+type Instruction = tuple[
+    command: Commands,
+    arg1: Argument,
+    arg2: Argument
+]
+
+proc day12*() =
+
+    let f = open(os.getAppDir() & "\\..\\input\\day12.txt")
+    defer: f.close()
+    var line : string
+
+    var program: seq[Instruction]
+
+    while f.read_line(line):
+
+        var m: RegexMatch
+        echo line
+        doAssert line.match(re"(\w+) (\w+)(?: (.*))?", m)
+
+        case m.groupFirstCapture(0,line):
+            of "cpy":
+                program.add((Copy,newArgument(m.groupFirstCapture(1,line)),newArgument(m.groupFirstCapture(2,line))))
+            of "inc":
+                program.add((Inc,newArgument(m.groupFirstCapture(1,line)),Argument(argType:None)))
+            of "dec":
+                program.add((Dec,newArgument(m.groupFirstCapture(1,line)),Argument(argType:None)))
+            of "jnz":
+                program.add((JNZ,newArgument(m.groupFirstCapture(1,line)),newArgument(m.groupFirstCapture(2,line))))
+            else:
+                doAssert false
+        
+    var IP = 0
+    while IP < program.len():
+        case program[IP].command:
+        of Copy:
+            registers[program[IP].arg2.register] = program[IP].arg1.getValue()
+            inc IP
+        of Inc:
+            inc registers[program[IP].arg1.register]
+            inc IP
+        of Dec:
+            dec registers[program[IP].arg1.register]
+            inc IP
+        of JNZ:
+            if program[IP].arg1.getValue() != 0:
+                IP += program[IP].arg2.getValue()
+            else:
+                inc IP
+
+    echo registers[0]
