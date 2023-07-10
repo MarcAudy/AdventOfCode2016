92905ff64001bafc7c5aa3d4673a213dff625963
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-09 23:16:04 -0700

    Commands for debug, release, and profiler

--- src/main.nim
+++ src/main.nim
@@ -1,11 +1,16 @@
 import day11
-#[import std/times
+
+#import nimprof 
+#[
+import std/times
 proc timeFunction(f: proc(), count: int = 1): float =
     let startTime = cpuTime()
     for i in 0 ..< count:
         f()
     let endTime = cpuTime()
     return endTime - startTime
+
+echo $timeFunction(day11.day11)
 ]#
 
 day11()
