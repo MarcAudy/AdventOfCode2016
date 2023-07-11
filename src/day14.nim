58bbebcaf15f5af4428a1795eff12796b9aeb20c
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-11 13:54:11 -0700

    Day 14 Part 2

--- src/day14.nim
+++ src/day14.nim
@@ -2,28 +2,45 @@ import re
 import std/md5
 import strutils
 
+#const DAY14_PART1 = 1
+const DAY14_PART2 = 1
+
 proc day14*() =
 
     #let salt = "abc"
     let salt = "zpqevtbw"
-    var index = -1
-    var keys: seq[int]
 
-    while keys.len() < 64:
+    when declared(DAY14_PART1):
+        proc getHash(index: int): string =
+            return getMD5(salt & $index)
+
+    when declared(DAY14_PART2):
+        var stretchedHashes: seq[string]
+        proc getHash(index: int): string =
+            if index >= stretchedHashes.len():
+                for i in stretchedHashes.len()..index:
+                    stretchedHashes.add(getMD5(salt & $i))
+                    for n in 1..2016:
+                        stretchedHashes[i] = getMD5(stretchedHashes[i])
+            return stretchedHashes[index]
+                
+    var index = -1
+    var foundKeys: int
+
+    while foundKeys < 64:
 
         inc index
 
-        let hash = getMD5(salt & $index)
-
+        let hash = getHash(index)
         let match = hash.find(re.re"(.)\1\1")
         if match != -1:
             let tripletChar = hash[match]
             let pentaChar = tripletChar&tripletChar&tripletChar&tripletChar&tripletChar
             block keycheck:
                 for i in index+1..index+1000:
-                    if getMD5(salt & $i).contains(pentaChar):
-                        keys.add(index)
-                        echo "Found Key ", keys.len(), ": Index=", index, " i=", i, " hash=", hash
+                    if getHash(i).contains(pentaChar):
+                        inc foundKeys
+                        echo "Found Key ", foundKeys, ": Index=", index, " i=", i, " hash=", hash
                         break keycheck
 
     echo index
