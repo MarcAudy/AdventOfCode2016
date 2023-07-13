a739c7ed6e8671748c9232e9566c77a08bdad98d
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-12 22:19:45 -0700

    Day 18 Part 2

--- src/day18.nim
+++ src/day18.nim
@@ -5,28 +5,42 @@ import sequtils
 #const rowCount = 10
 
 # Part 1
+#const firstRow = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^"
+#const rowCount = 40
+
+# Part 2
 const firstRow = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^"
-const rowCount = 40
+const rowCount = 400000
 
 const rowWidth = len(firstRow)
 
 proc day18*() =
 
-    var grid: array[rowCount, array[rowWidth, bool]]
+    var curRow: array[rowWidth, bool]
 
     for i in 0..<rowWidth:
-        grid[0][i] = firstRow[i] == '^'
+        curRow[i] = firstRow[i] == '^'
 
-    for row in 1..<rowCount:
+    var safeCount = count(curRow,false)
+
+    var row = 1
+    while row < rowCount:
+        var nextRow: array[rowWidth, bool]
         for i in 0..<rowWidth:
-            let lTrap = i > 0 and grid[row-1][i-1]
-            let cTrap = grid[row-1][i]
-            let rTrap = i < rowWidth-1 and grid[row-1][i+1]
+            let lTrap = i > 0 and curRow[i-1]
+            let cTrap = curRow[i]
+            let rTrap = i < rowWidth-1 and curRow[i+1]
 
-            grid[row][i] = ((lTrap and not cTrap and not rTrap) or
-                            (rTrap and not cTrap and not lTrap) or
-                            (lTrap and cTrap and not rTrap) or
-                            (rTrap and cTrap and not lTrap))
+            nextRow[i] = ((lTrap and not cTrap and not rTrap) or
+                          (rTrap and not cTrap and not lTrap) or
+                          (lTrap and cTrap and not rTrap) or
+                          (rTrap and cTrap and not lTrap))
 
-    echo foldl(grid, a + count(b,false), 0)
+            if not nextRow[i]:
+                inc safeCount
+
+        inc row
+        curRow = nextRow
+
+    echo safeCount
                 
