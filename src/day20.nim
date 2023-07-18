62e05b840e0597e9adf544b66be5d6c7560d780e
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-18 11:45:12 -0700

    Day 20 Part 1

---
+++ src/day20.nim
@@ -0,0 +1,35 @@
+import algorithm
+import os
+import regex
+import strutils
+
+type Range = tuple[
+    min: int,
+    max: int
+]
+
+proc compareRanges(r1: Range, r2: Range): int =
+    return cmp(r1.min, r2.min)
+
+proc day20*() =
+ 
+    let f = open(os.getAppDir() & "\\..\\input\\day20.txt")
+    defer: f.close()
+    var line : string
+
+    var ranges: seq[Range]
+
+    while f.read_line(line):
+        var m: RegexMatch
+        doAssert line.match(re"(\d+)-(\d+)", m)
+        let nextRange: Range = (parseInt(m.groupFirstCapture(0,line)), parseInt(m.groupFirstCapture(1,line)))
+        ranges.insert(nextRange, ranges.lowerBound(nextRange, compareRanges))
+
+    var validIP = 0
+    for r in ranges:
+        if validIP < r.min:
+            break
+        if validIP <= r.max:
+            validIP = r.max + 1
+
+    echo validIP
