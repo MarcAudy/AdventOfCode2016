6184d5fe437943c87d1177a0c63c5dd42385aa67
Marc Audy <MarcAudy@users.noreply.github.com>
2023-07-19 11:11:54 -0700

    Day 21 Part 2

--- src/day21.nim
+++ src/day21.nim
@@ -2,22 +2,37 @@ import algorithm
 import os
 import regex
 import strutils
+import tables
 
 proc day21*() =
  
+    const part1 = false
+
     # Sample
-    #const fileName = "day21_sample.txt"
-    #var passcode = "abcde"
+    #const fileName = if part1: "day21_sample.txt" else: "day21.txt"
+    #var passcode = if part1: "abcde" else: "bgfacdeh"
 
     # Part 1
     const fileName = "day21.txt"
-    var passcode = "abcdefgh"
+    var passcode = if part1: "abcdefgh" else: "fbgdceah"
+
+    if not part1:
+        # Part2 only works for the 8 digit case
+        doAssert len(passcode) == 8
+    const PART2_ROTATE_MAP = {0:1, 1:1, 2: -2, 3:2, 4: -1, 5:3, 6:0, 7:4}.toTable
 
     let f = open(os.getAppDir() & "\\..\\input\\" & fileName)
     defer: f.close()
     var line : string
 
+    var lines: seq[string]
     while f.read_line(line):
+        lines.add(line)
+
+    var i = if part1: 0 else: high(lines)
+    while i in 0..high(lines):
+        line = lines[i]
+
         var m: RegexMatch
         
         if line.match(re"swap position (\d+) with position (\d+)", m):
@@ -35,15 +50,19 @@ proc day21*() =
 
         elif line.match(re"rotate (left|right) (\d+) steps?", m):
             let steps = parseInt(m.groupFirstCapture(1,line))
-            if m.groupFirstCapture(0,line) == "left":
+            if m.groupFirstCapture(0,line) == (if part1: "left" else: "right"):
                 passcode.rotateLeft(steps)
             else:
                 passcode.rotateLeft(-steps)
 
         elif line.match(re"rotate based on position of letter (.)", m):
             let p = passcode.find(m.groupFirstCapture(0,line)[0])
-            let steps = p + 1 + (if p >= 4: 1 else: 0)
-            passcode.rotateLeft(-steps)
+            if part1:
+                let steps = p + 1 + (if p >= 4: 1 else: 0)
+                passcode.rotateLeft(-steps)
+            else:
+                let steps = PART2_ROTATE_MAP[p]
+                passcode.rotateLeft(steps)
 
         elif line.match(re"reverse positions (\d+) through (\d+)", m):
             var p1 = parseInt(m.groupFirstCapture(0,line))
@@ -54,8 +73,8 @@ proc day21*() =
                 dec p2
 
         elif line.match(re"move position (\d+) to position (\d+)", m):
-            let p1 = parseInt(m.groupFirstCapture(0,line))
-            let p2 = parseInt(m.groupFirstCapture(1,line))
+            let p1 = parseInt(m.groupFirstCapture((if part1: 0 else: 1),line))
+            let p2 = parseInt(m.groupFirstCapture((if part1: 1 else: 0),line))
             let ch = passcode[p1]
             passcode.delete(p1..p1)
             passcode.insert($ch, p2)
@@ -63,5 +82,10 @@ proc day21*() =
         else:
             echo line
             doAssert false
+
+        if part1:
+            inc i
+        else:
+            dec i
 
     echo passcode
